b198173922d873b8250b26aa31c1b8c94c147cdf
Ahmedhassan120478 <rajahassan120478@gmail.com>
2024-11-28 14:15:22 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 pwd
 ls
-# Ensure yq is available
-if ! command -v yqq &> /dev/null; then
-    echo "yqq is not installed. Exiting."
-    exit 1
-fi
+cd helm
+yqq eval '.img = "new-image-name:latest"' -i values.yaml
+echo "${commitId}"
 
-# Read the new image tag from the argument
-NEW_IMAGE_TAG=$1
 
-# Update the Helm values.yaml file
-cd helm || exit 1
-yqq eval ".img = \"${NEW_IMAGE_TAG}\"" -i values.yaml
-
-# Output the updated values.yaml for verification
-echo "Updated values.yaml:"
-cat values.yaml
