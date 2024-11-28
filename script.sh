74a1665a0d11657ce4eb6be08ae57c7061fce8b8
Ahmedhassan120478 <rajahassan120478@gmail.com>
2024-11-28 15:07:40 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 pwd
 ls
 cd helm
-yq eval ".image.tag = \"${DOCKER_IMAGE}:${commitId}\"" -i values.yaml
+yqq eval ".image.tag = \"${DOCKER_IMAGE}:${commitId}\"" -i values.yaml
 echo "${commitId}"
 
 
