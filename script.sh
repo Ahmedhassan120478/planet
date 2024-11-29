6bd9b649d52f13a6f5a20a09af65f80f7104e486
Ahmedhassan120478 <rajahassan120478@gmail.com>
2024-11-29 14:59:49 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,5 +6,7 @@ echo "DOCKER_IMAGE=${DOCKER_IMAGE}"
 echo "commitId=${commitId}"
 yqq eval ".image.tag = \"${commitId}-abc\"" -i values.yaml
 echo "${commitId}"
+sh "helm upgrade nginx ./helm --namespace hassan --install"
+
 
 
